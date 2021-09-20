cb0a6f83a60d246d992d47c1cbb603c4f2df72ca
Miguel Vitela <miguel55020@gmail.com>
2021-09-20 17:18:34 -0500

    Mi primera versión

---
+++ Strings/4.4_what_is_a_string.py
@@ -0,0 +1,18 @@
+""" Para imprimir un string se pueden usar comillas sencillas o dobles, un ejemplo de beneficios al
+combinarlos es el siguiente: """
+
+print('Ella me dijo "hola"')
+
+# También se puede imprimir un string asiganando a una variable y usando 3 dobles comillas
+m = """
+HOLAA
+  PUTOSS
+"""
+
+print(m)
+
+# Python también tiene una función que muestra a que tipo de clase pertenece una variable, por ejemplo:
+
+Nombre = 'Miguel'
+print(type(Nombre))
+
